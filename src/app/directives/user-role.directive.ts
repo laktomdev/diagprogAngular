e3a93d044bbe0d0d6eac22baa771f94668eca4c8
Tomek <tomasz.lakomy@elprosys.com>
2019-11-12 15:54:32 +0100

    poprawki w dyrektywie

--- src/app/directives/user-role.directive.ts
+++ src/app/directives/user-role.directive.ts
@@ -1,21 +1,26 @@
-import { Directive, ElementRef, OnInit, Input, Renderer } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input, Renderer, Renderer2 } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Directive({
   selector: '[appUserRole]'
 })
 export class UserRoleDirective implements OnInit {
-
-  @Input() roleName: string;
+  @Input() roleNames: string;
   jwtHelper = new JwtHelperService();
 
-  constructor(private elementRef: ElementRef, private render: Renderer) {
-   }
+  constructor(private elementRef: ElementRef, private render: Renderer2) {}
 
-   ngOnInit() {
-      const roleInToken = this.jwtHelper.decodeToken(localStorage.getItem('token')).role;
-      if (roleInToken !== this.roleName) {
-        this.render.setElementStyle(this.elementRef.nativeElement, 'visibility', 'hidden');
-      }
+  ngOnInit() {
+    const roles = this.roleNames.split(',');
+    const roleInToken = this.jwtHelper.decodeToken(
+      localStorage.getItem('token')
+    ).role;
+    if (!roles.includes(roleInToken)) {
+      this.render.setStyle(
+        this.elementRef.nativeElement,
+        'visibility',
+        'hidden'
+      );
     }
+  }
 }
