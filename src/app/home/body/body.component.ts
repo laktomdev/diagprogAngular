fb594ce238aba43a08f071332414fafb38f4ca6f
Tomek <tomasz.lakomy@elprosys.com>
2019-11-12 14:22:05 +0100

    zz

--- src/app/home/body/body.component.ts
+++ src/app/home/body/body.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { slideInAnimation } from 'src/app/animations';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from 'src/app/authorization/auth.service';
 
 @Component({
   selector: 'app-body',
@@ -10,7 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  constructor(public aS: AuthService) { }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
