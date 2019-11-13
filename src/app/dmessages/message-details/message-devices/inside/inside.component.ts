572a861aa53434a8b39d1c1521f7bb61a1a7ca5b
Tomek <tomasz.lakomy@elprosys.com>
2019-11-13 15:49:39 +0100

    alerty dla dodania komunikatu dla urządzeń

--- src/app/dmessages/message-details/message-devices/inside/inside.component.ts
+++ src/app/dmessages/message-details/message-devices/inside/inside.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DeviceShort } from 'src/app/models/Device/deviceShort';
 import { MessagesService } from 'src/app/services/messages.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-inside',
@@ -21,17 +22,30 @@ export class InsideComponent implements OnInit {
     this.selected = selected;
   }
 
+  askIfRemove() {
+    this.alertify.confirm('Potwierdź usunięcie z urządzeń',
+     `Czy chcesz usunąć komunikat z ${this.selected.length} urządzeń?`,
+      () => this.removeFromSelected());
+  }
   removeFromSelected() {
     this.loading = true;
     this.mS.removeMessageFromMultipleDevices(this.selected.map(x => x.id), this.messageId).subscribe((data) => {
-      this.itemsRemovedEmitter.emit(data);
+      if (data === this.selected.length) {
+        this.itemsRemovedEmitter.emit(data);
+        this.alertify.success(`Usunięto komunikat z ${this.selected.length} urządzeń`);
+        this.selected = undefined;
+      } else {
+        if (data > 0) {
+          this.alertify.success(`Usunięto komunikat z ${data} urządzeń`);
+        }
+        this.alertify.error(`Nie udało się usunąć komunikatu z ${this.selected.length - data} z ${this.selected.length} urządzeń.`);
+      }
       this.loading = false;
-      this.selected = undefined;
     });
   }
 
   // TODO: tutaj też pobierać listę zamiast Inputu
-  constructor(private mS: MessagesService) { }
+  constructor(private mS: MessagesService, private alertify: AlertifyService) { }
 
 
   ngOnInit() {
--- src/app/dmessages/message-details/message-devices/outside/outside.component.ts
+++ src/app/dmessages/message-details/message-devices/outside/outside.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { DeviceShort } from 'src/app/models/Device/deviceShort';
 import { MessagesService } from 'src/app/services/messages.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-outside',
@@ -17,19 +18,34 @@ export class OutsideComponent implements OnInit {
   selected: DeviceShort[];
   loading = false;
 
-  constructor(private mS: MessagesService) { }
+  constructor(private mS: MessagesService, private alertify: AlertifyService) { }
 
   onSelectionChanged(selected: DeviceShort[]) {
     this.selected = selected;
   }
 
+  askIfAdd() {
+    this.alertify.confirm('Potwierdź dodanie do urządzeń',
+    `Czy chcesz dodać komunikat do ${this.selected.length} urządzeń?`,
+     () => this.addToSelected());
+  }
+
   addToSelected() {
     this.loading = true;
     this.mS.addMessageToMultipleDevices(this.selected.map(x => x.id), this.messageId).subscribe((data => {
-      this.itemsAddedEmitter.emit(data);
-      this.loading = false;
-      this.selected = undefined;
+      console.log(data);
+      if (data === this.selected.length) {
+        this.itemsAddedEmitter.emit(data);
+        this.alertify.success(`Dodano komunikat do ${this.selected.length} urządzeń`);
+        this.selected = undefined;
+      } else {
+        if (data > 0) {
+          this.alertify.success(`Dodano komunikat do ${data} urządzeń`);
+        }
+        this.alertify.error(`Nie udało się dodać komunikatu do ${this.selected.length - data} z ${this.selected.length} urządzeń`);
+      }
 
+      this.loading = false;
     }));
   }
 
