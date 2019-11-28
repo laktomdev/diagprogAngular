cc15b2acac79cdffa9564e6b4af7a4d62e4a5ef1
Tomek <tomasz.lakomy@elprosys.com>
2019-11-28 13:17:45 +0100

    dodano export do Excela. DeviceShort zmiany

--- src/app/devices/device-list-expanded/device-list-expanded.component.ts
+++ src/app/devices/device-list-expanded/device-list-expanded.component.ts
@@ -1,7 +1,25 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
-import { MatTableDataSource, MatPaginator, MatDialog, MatSort } from '@angular/material';
-import { Device } from 'src/app/models/Device/device';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import {
+  MatTableDataSource,
+  MatPaginator,
+  MatSort
+} from '@angular/material';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 import { DeviceShort } from 'src/app/models/Device/deviceShort';
 import { DevicesService } from 'src/app/services/devices.service';
 
@@ -11,33 +29,48 @@ import { DevicesService } from 'src/app/services/devices.service';
   styleUrls: ['./device-list-expanded.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      )
+    ])
+  ]
 })
 export class DeviceListExpandedComponent implements OnInit, OnChanges {
-
-
   dataSource: MatTableDataSource<DeviceShort>;
 
-  columnsToDisplay = ['deviceNumber', 'seller.name', 'customer.name', 'packageName', 'timeLock', 'language', 'lastActivation'];
+  columnsToDisplay = [
+    'deviceNumber',
+    'seller.name',
+    'customer.name',
+    'packageName',
+    'timeLock',
+    'language',
+    'lastActivation'
+  ];
   expandedElement: DeviceShort | null;
 
-  constructor(private dS: DevicesService) { }
+  constructor(private dS: DevicesService) {}
 
   @Input() devices: DeviceShort[];
 
   @Input() expandId: number;
   @Output() refreshListEmitter = new EventEmitter<number>();
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+          // ustawienie filtrowania wgłąb obiektu https://stackoverflow.com/a/57747792
+      // tslint:disable-next-line:no-shadowed-variable
+      this.dataSource.filterPredicate = (data: any, filter) => {
+        const dataStr = JSON.stringify(data).toLowerCase();
+        return dataStr.indexOf(filter) !== -1;
+      };
+
+      this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   refreshListTriggered(deviceIdent: number) {
@@ -46,7 +79,8 @@ export class DeviceListExpandedComponent implements OnInit, OnChanges {
 
   sortingDataAccessor(item, property) {
     if (property.includes('.')) {
-      return property.split('.')
+      return property
+        .split('.')
         .reduce((object, key) => object[key] || '', item);
     }
     return item[property];
@@ -54,28 +88,22 @@ export class DeviceListExpandedComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     if (!this.dataSource) {
-
-      this.dataSource =  new MatTableDataSource<DeviceShort>(this.devices);
+      this.dataSource = new MatTableDataSource<DeviceShort>(this.devices);
       this.dataSource.sort = this.sort;
       this.dataSource.sortingDataAccessor = this.sortingDataAccessor;
       this.dataSource.paginator = this.paginator;
 
-      // ustawienie filtrowania wgłąb obiektu https://stackoverflow.com/a/57747792
-      // tslint:disable-next-line:no-shadowed-variable
-      this.dataSource.filterPredicate = (data: any, filter) => {
-        const dataStr = JSON.stringify(data).toLowerCase();
-        return dataStr.indexOf(filter) !== -1;
-      };
+
     } else {
       this.dataSource.data = this.devices;
     }
 
     if (this.expandId) {
-      this.expandedElement = this.dataSource.data.find(x => x.id === this.expandedElement.id);
+      this.expandedElement = this.dataSource.data.find(
+        x => x.id === this.expandedElement.id
+      );
     }
   }
 
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
