d8fd6bc3dbd606997f74478276be04d62ceb8b39
Tomek <tomasz.lakomy@elprosys.com>
2019-11-28 16:48:35 +0100

    wyszukiwanie po języku

--- src/app/tableTools/device-table-column-filter/device-table-column-filter.component.ts
+++ src/app/tableTools/device-table-column-filter/device-table-column-filter.component.ts
@@ -24,16 +24,25 @@ export class DeviceTableColumnFilterComponent implements OnInit {
   customerOptions: string[];
   filteredCustomerOptions: Observable<string[]>;
 
+  languageControl = new FormControl();
+  languageOptions: string[];
+  filteredLanguageOptions: Observable<string[]>;
+
+
   tableFilters = [
     {id: 'sellerName', value: ''},
-    {id: 'customerName', value: ''}
+    {id: 'customerName', value: ''},
+    {id: 'language', value: ''}
   ];
 
   ngOnInit() {
-    this.sellerOptions = Array.from(new Set(this.dataSource.data.map(x => x.sellerName.trim())));
-    this.customerOptions = Array.from(new Set(this.dataSource.data.map(x => x.customerName.trim())));
+    this.sellerOptions = Array.from(new Set(this.dataSource.data.filter(x => x.sellerName).map(x => x.sellerName.trim()))).sort();
+    this.customerOptions = Array.from(new Set(this.dataSource.data.filter(x => x.customerName).map(x => x.customerName.trim()))).sort();
+    this.languageOptions = Array.from(new Set(this.dataSource.data.filter(x => x.language).map(x => x.language.trim()))).sort();
+
     this.sellerOptions.unshift('');
     this.customerOptions.unshift('');
+    this.languageOptions.unshift('');
 
     this.filteredSellerOptions = this.sellerControl.valueChanges.pipe(
       startWith(''),
@@ -43,6 +52,11 @@ export class DeviceTableColumnFilterComponent implements OnInit {
     this.filteredCustomerOptions = this.customerControl.valueChanges.pipe(
       startWith(''),
       map(value => this._customerFilter(value))
+    );
+
+    this.filteredLanguageOptions = this.languageControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._languageFilter(value))
     );
 
 
@@ -68,6 +82,18 @@ export class DeviceTableColumnFilterComponent implements OnInit {
         this.applyFilter( JSON.stringify(this.tableFilters));
       }
     );
+
+    this.filteredLanguageOptions.subscribe(
+      () => {
+        if (this.languageControl.value) {
+
+          this.tableFilters.find(x => x.id === 'language').value = this.languageControl.value;
+        } else {
+          this.tableFilters.find(x => x.id === 'language').value = '';
+        }
+        this.applyFilter(JSON.stringify(this.tableFilters));
+      }
+    );
   }
 
   private _sellerFilter(value: string): string[] {
@@ -79,6 +105,12 @@ export class DeviceTableColumnFilterComponent implements OnInit {
     const filterValue = value.toLowerCase();
 
     return this.customerOptions.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  private _languageFilter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.languageOptions.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
   applyFilter(filterValue: string) {
